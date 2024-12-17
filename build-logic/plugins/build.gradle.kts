440ef3dedbe6af1d1649550792dabb471048de82
denglongfei <674179921@qq.com>
2024-12-17 15:45:25 +0800

    Bug fixes

--- build-logic/plugins/build.gradle.kts
+++ build-logic/plugins/build.gradle.kts
@@ -48,8 +48,8 @@ description = "activityGuard"
 version = "1.0.0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 publishing {
     publications {
